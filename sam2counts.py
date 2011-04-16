c5238d5a894d6bbd95c802ce81b6dc375eabb075
Vince Buffalo <vsbuffalo@ucdavis.edu>
2011-04-15 18:43:31 -0700

    nonunique/unique mapper output, option for all of references output (even with 0 counts) and BAM support added

--- sam2counts.py
+++ sam2counts.py
@@ -7,6 +7,8 @@ Author: Vince Buffalo
 Email: vsbuffaloAAAAA@gmail.com (with poly-A tail removed)
 """
 
+VERSION = 0.91
+
 import sys
 import csv
 from os import path
@@ -17,7 +19,7 @@ except ImportError:
 
 from optparse import OptionParser
 
-def SAM_file_to_counts(filename):
+def SAM_file_to_counts(filename, bam=False, extra=False, use_all_references=True):
     """
     Take SAM filename, and create a hash of mapped and unmapped reads;
     keys are reference sequences, values are the counts of occurences.
@@ -26,28 +28,46 @@ def SAM_file_to_counts(filename):
     is output, which is handy for diagnostics.
     """
     counts = dict()
-    qual_counts = dict()
-    sf = pysam.Samfile(filename, 'r')
+    unique = dict()
+    nonunique = dict()
+    mode = 'r'
+    if bam:
+        mode = 'rb'
+    sf = pysam.Samfile(filename, mode)
+
+    if use_all_references:
+        # Make dictionary of all entries in header
+        for sn in sf.header['SQ']:
+            if extra:
+                unique[sn['SN']] = 0
+                nonunique[sn['SN']] = 0
+            counts[sn['SN']] = 0
+
     for read in sf:
-        id_name = sf.getrname(read.rname) if read.rname != -1 else 0
-
-        ## quality recording
-        if not qual_counts.get(id_name, False):
-            # create empty  hash
-            qual_counts[id_name] = {'0':0, '>0':0}
-
-        ## initiate entry; even if not mapped, record 0 count
-        counts[id_name] = counts.get(id_name, 0)
-        
         if not read.is_unmapped:
+            id_name = sf.getrname(read.rname) if read.rname != -1 else 0
+
+            if not use_all_references and not counts.get(id_name, False):
+                ## Only make keys based on aligning reads, make empty hash
+                if extra:
+                    unique[id_name] = 0
+                    nonunique[id_name] = 0
+                ## initiate entry; even if not mapped, record 0 count
+                counts[id_name] = counts.get(id_name, 0)
+        
+            
             counts[id_name] = counts.get(id_name, 0) + 1
 
-            if read.mapq == 0:
-                qual_counts[id_name]['0'] = qual_counts[id_name]['0'] + 1
-            else:
-                qual_counts[id_name]['>0'] = qual_counts[id_name]['>0'] + 1
+            if extra:
+                if read.mapq == 0:
+                    nonunique[id_name] = nonunique[id_name] + 1
+                else:
+                    unique[id_name] = unique[id_name] + 1
 
-    return {'counts':counts, 'qual_counts':qual_counts}
+    if extra:
+        return {'counts':counts, 'unique':unique, 'nonunique':nonunique}
+
+    return {'counts':counts}
 
 def collapsed_nested_count_dict(counts_dict, all_ids, order=None):
     """
@@ -106,12 +126,21 @@ if __name__ == '__main__':
     parser.add_option("-d", "--delimiter", dest="delimiter",
                       help="the delimiter (default: tab)", default='\t')
     parser.add_option("-o", "--out-file", dest="out_file",
-                      help="the output file's name (default: counts.txt)",
-                      default='counts.txt')
-    ## Output not yet implemented
-    # parser.add_option("-q", "--include-quality", dest="quality",
-    #                   help="generate quality statistics too (default: False)",
-    #                   default=False)
+                      help="output filename (default: counts.txt)",
+                      default='counts.txt', action="store", type="string")
+    parser.add_option("-u", "--extra-output", dest="extra_out",
+                      help="output extra information on non-unique and unique mappers (default: False)",
+                      default=False, action="store_true")
+    parser.add_option("-b", "--bam", dest="bam",
+                      help="all input files are BAM (default: False)", 
+                      default=False, action="store_true")
+    parser.add_option("-r", "--use-all-references", dest="use_all_references",
+                      help="Use all the references from the SAM header (default: True)",
+                      default=True, action="store_false")
+    parser.add_option("-f", "--extra-out-files", dest="extra_out_files",
+                      help="comma-delimited filenames of unique and non-unique output "
+                      "(default: unique.txt,nonunique.txt)",
+                      default='unique.txt,nonunique.txt', action="store", type="string")
     parser.add_option("-v", "--verbose", dest="verbose",
                       help="enable verbose output")
 
@@ -121,22 +150,38 @@ if __name__ == '__main__':
         parser.error("one or more SAM files as arguments required")
 
     file_counts = dict()
-    file_qual_counts = dict()
+    file_unique_counts = dict()
+    file_nonunique_counts = dict()
     all_ids = list()
     files = [path.basename(f) for f in args]
 
     if len(set(files)) != len(set(args)):
         parser.error("file args must have unique base names (i.e. no foo/bar joo/bar)")
+
+    ## do a pre-run check that all files exist
+    for full_filename in args:
+        if not path.exists(full_filename):
+            parser.error("file '%s' does not exist" % full_filename)
         
     for full_filename in args:
         filename = path.basename(full_filename)
-        ## read in SAM file, extract counts, and unpack counts and qual_counts
-        tmp = SAM_file_to_counts(full_filename)
-        counts, qual_counts = tmp['counts'], tmp['qual_counts']
+        ## read in SAM file, extract counts, and unpack counts
+        tmp = SAM_file_to_counts(full_filename, bam=options.bam, extra=options.extra_out,
+                                 use_all_references=options.use_all_references)
 
-        ## save counts, qual_counts, and all ids encountered
+        if options.extra_out:
+            counts, unique, nonunique = tmp['counts'], tmp['unique'], tmp['nonunique']
+        else:
+            counts = tmp['counts']
+
+        ## save counts, and unique/non-unique counts
         file_counts[filename] = counts
-        file_qual_counts[filename] = qual_counts
+
+        if options.extra_out:
+            file_unique_counts[filename] = unique
+            file_nonunique_counts[filename] = nonunique
+
+        ## add all ids encountered in this in this file
         all_ids.extend(file_counts[filename].keys())
 
     ## Uniquify all_ids, and then take the nested file_counts
@@ -144,3 +189,12 @@ if __name__ == '__main__':
     all_ids = set(all_ids)
     table_dict = collapsed_nested_count_dict(file_counts, all_ids, order=files)
     counts_to_file(table_dict, options.out_file, delimiter=options.delimiter)
+
+    if options.extra_out:
+        unique_fn, nonunique_fn = options.extra_out_files.split(',')
+        unique_table_dict = collapsed_nested_count_dict(file_unique_counts, all_ids, order=files)
+        nonunique_table_dict = collapsed_nested_count_dict(file_nonunique_counts, all_ids, order=files)
+        
+        counts_to_file(unique_table_dict, unique_fn, delimiter=options.delimiter)
+        counts_to_file(nonunique_table_dict, nonunique_fn, delimiter=options.delimiter)
+        
